7cd9e8cd4a2a95e1eb6dccd5f517e0516fd7c65a
SabarishCodeWizard <ravik60656@gmail.com>
2024-08-20 17:48:09 +0530

    20 08 2024 automation

--- automation.ts
+++ automation.ts
@@ -2,34 +2,34 @@ import { test, expect } from '@playwright/test';
 
 test('successful login', async ({ page }) => {
 
-  await page.goto('https://tamiltranslator.pythonanywhere.com/login');
+    await page.goto('https://tamiltranslator.pythonanywhere.com/login');
 
 
-  await page.fill('input[name="username"]', 'test55');
+    await page.fill('input[name="username"]', 'test55');
 
-  await page.click('button[type="submit"]');
-
-
-  await expect(page).toHaveURL('https://tamiltranslator.pythonanywhere.com/');
+    await page.click('button[type="submit"]');
 
 
-  await page.fill('textarea[name="text"]', 'After translating the text, clear the history. If you try to translate again, it still shows the previous text.');
+    await expect(page).toHaveURL('https://tamiltranslator.pythonanywhere.com/');
 
 
-  await page.selectOption('select[name="src_lang"]', 'en');
-
-  await page.selectOption('select[name="dest_lang"]', 'ta');
+    await page.fill('textarea[name="text"]', 'After translating the text, clear the history. If you try to translate again, it still shows the previous text.');
 
 
-  await page.click('button[type="submit"]');
+    await page.selectOption('select[name="src_lang"]', 'en');
+
+    await page.selectOption('select[name="dest_lang"]', 'ta');
 
 
-  const translation = page.locator('#translatedText');
-
-  await expect(translation).toHaveText('உரையை மொழிபெயர்த்த பிறகு, வரலாற்றை அழிக்கவும்.நீங்கள் மீண்டும் மொழிபெயர்க்க முயற்சித்தால், அது முந்தைய உரையை இன்னும் காட்டுகிறது.');
-
-  console.log('Translation test passed!');
+    await page.click('button[type="submit"]');
 
 
-  console.log('Login test passed!');
+    const translation = page.locator('#translatedText');
+
+    await expect(translation).toHaveText('உரையை மொழிபெயர்த்த பிறகு, வரலாற்றை அழிக்கவும்.நீங்கள் மீண்டும் மொழிபெயர்க்க முயற்சித்தால், அது முந்தைய உரையை இன்னும் காட்டுகிறது.');
+
+    console.log('Translation test passed!');
+
+
+    console.log('Login test passed!');
 });
